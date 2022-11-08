b1c78c58c6c08b7ee4b7093e298019ffdd6beb04
tair836 <tair15shriki@gmail.com>
2022-11-08 10:23:21 +0200

    Update post.test.js

--- test/post.test.js
+++ test/post.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const app = require('../../server')
+const app = require('../server')
 const mongoose = require('mongoose')
 const Post = require('../models/post_model')
 
