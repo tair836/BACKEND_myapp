8d634b4e25e88fbc757f38d2d54396480b514b26
Tair Shriki <tair15shriki@gmail.com>
2022-12-21 15:17:22 +0200

    9
    chat using socket io

--- dist/src/tests/socket.test.js
+++ dist/src/tests/socket.test.js
@@ -20,57 +20,78 @@ const post_model_1 = __importDefault(require("../models/post_model"));
 const user_model_1 = __importDefault(require("../models/user_model"));
 const userEmail = "user1@gmail.com";
 const userPassword = "12345";
-let accessToken = '';
-let clientSocket;
-function clientSocketConnect() {
+const userEmail2 = "user2@gmail.com";
+const userPassword2 = "12345";
+let client1;
+let client2;
+function clientSocketConnect(clientSocket) {
     return new Promise((resolve) => {
         clientSocket.on("connect", () => {
             resolve("1");
         });
     });
 }
+const connectUser = (userEmail, userPassword) => __awaiter(void 0, void 0, void 0, function* () {
+    const response1 = yield (0, supertest_1.default)(app_1.default).post('/auth/register').send({
+        "email": userEmail,
+        "password": userPassword
+    });
+    const userId = response1.body._id;
+    const response = yield (0, supertest_1.default)(app_1.default).post('/auth/login').send({
+        "email": userEmail,
+        "password": userPassword
+    });
+    const token = response.body.accessToken;
+    const socket = (0, socket_io_client_1.default)('http://localhost:' + process.env.PORT, {
+        auth: {
+            token: 'barrer ' + token
+        }
+    });
+    yield clientSocketConnect(socket);
+    const client = { socket: socket, accessToken: token, id: userId };
+    return client;
+});
 describe("my awesome project", () => {
+    jest.setTimeout(15000);
     beforeAll(() => __awaiter(void 0, void 0, void 0, function* () {
         yield post_model_1.default.remove();
         yield user_model_1.default.remove();
-        yield (0, supertest_1.default)(app_1.default).post('/auth/register').send({
-            "email": userEmail,
-            "password": userPassword
-        });
-        const response = yield (0, supertest_1.default)(app_1.default).post('/auth/login').send({
-            "email": userEmail,
-            "password": userPassword
-        });
-        accessToken = response.body.accessToken;
-        clientSocket = (0, socket_io_client_1.default)('http://localhost:' + process.env.PORT, {
-            auth: {
-                token: 'barrer ' + accessToken
-            }
-        });
-        yield clientSocketConnect();
+        client1 = yield connectUser(userEmail, userPassword);
+        client2 = yield connectUser(userEmail2, userPassword2);
+        console.log("finish beforeAll");
     }));
     afterAll(() => {
+        client1.socket.close();
+        client2.socket.close();
         app_1.default.close();
-        clientSocket.close();
         mongoose_1.default.connection.close();
     });
     test("should work", (done) => {
-        clientSocket.once("echo:echo_res", (arg) => {
-            console.log("on any");
+        client1.socket.once("echo:echo_res", (arg) => {
+            console.log("echo:echo");
             expect(arg.msg).toBe('hello');
-            clientSocket.removeAllListeners();
             done();
         });
-        clientSocket.emit("echo:echo", { 'msg': 'hello' });
+        client1.socket.emit("echo:echo", { 'msg': 'hello' });
     });
     test("Post get all test", (done) => {
-        clientSocket.once('post:get_all', (arg) => {
-            console.log("on any");
+        client1.socket.once('post:get_all', (arg) => {
+            console.log("on any " + arg);
             expect(arg.status).toBe('OK');
-            clientSocket.removeAllListeners();
             done();
         });
-        clientSocket.emit('post:get_all');
+        console.log(" test post get all");
+        client1.socket.emit("post:get_all", "stam");
+    });
+    test("Test chat messages", (done) => {
+        const message = "hi... test 123";
+        client2.socket.once('chat:message', (args) => {
+            expect(args.to).toBe(client2.id);
+            expect(args.message).toBe(message);
+            expect(args.from).toBe(client1.id);
+            done();
+        });
+        client1.socket.emit("chat:send_message", { 'to': client2.id, 'message': message });
     });
 });
 //# sourceMappingURL=socket.test.js.map
